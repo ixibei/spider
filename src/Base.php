58ecf31415d09b92ff22cf93ee2a4179087b9f15
ixibei <993870545@qq.com>
2021-12-15 11:36:35 +0800

    f

--- src/Base.php
+++ src/Base.php
@@ -255,7 +255,7 @@ class Base
                         $proxyIp = json_encode($jsonArr);
                         Cache::set('spider:proxy:ip',$proxyIp,$deadline);
                         $filePath = __DIR__.'/Cache/proxy.txt';
-                        file_put_contents($filePath,date('Y-m-d H:i:s').' : '.$proxyIp,FILE_APPEND);
+                        file_put_contents($filePath,date('Y-m-d H:i:s').' : '.$proxyIp."\r\n",FILE_APPEND);
                         break;
                     }
                 }
